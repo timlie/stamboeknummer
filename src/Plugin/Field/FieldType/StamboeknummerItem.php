d44ac6f2aacc2b6a5f989a5e50d7dbd024cbd84d
Tim Lievens <timlievens@gmail.com>
2016-11-07 20:19:28 +0100

    Added @FieldType annotation

---
+++ src/Plugin/Field/FieldType/StamboeknummerItem.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Plugin implementation of the 'stamboeknummer' field type.
+ *
+ * @FieldType(
+ *   id = "stamboeknummer",
+ *   label = @Translation("Stamboeknummer for education"),
+ *   description = @Translation("Stamboeknummer field for education employees."),
+ *   category = @Translation("Custom"),
+ *   default_widget = "stamboeknummer_default_widget",
+ *   default_formatter = "stamboeknummer_default_formatter"
+ * )
+ */
+
+namespace Drupal\stamboeknummer\Plugin\Field\FieldType;
+
+
+class StamboeknummerItem {
+
+}
