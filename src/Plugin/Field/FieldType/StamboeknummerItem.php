73b0709b3509945d18a227a21c10f33108489191
Tim Lievens <timlievens@gmail.com>
2016-11-07 20:50:30 +0100

    Added StamboeknummerItem class

--- src/Plugin/Field/FieldType/StamboeknummerItem.php
+++ src/Plugin/Field/FieldType/StamboeknummerItem.php
@@ -1,5 +1,11 @@
 <?php
 
+namespace Drupal\stamboeknummer\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldItemBase;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
 /**
  * Plugin implementation of the 'stamboeknummer' field type.
  *
@@ -12,10 +18,50 @@
  *   default_formatter = "stamboeknummer_default_formatter"
  * )
  */
+class StamboeknummerItem extends FieldItemBase {
+  /**
+   * Field type schema definition.
+   *
+   * Inside this method we define the database schema used to store data for
+   * our field type.
+   *
+   * Here there is a list of allowed column types: https://goo.gl/YY3G7s
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    return array(
+      'columns' => array(
+        'value' => array(
+          'type' => 'int',
+          'size' => 'big',
+          'not null' => FALSE,
+        ),
+      ),
+    );
+  }
 
-namespace Drupal\stamboeknummer\Plugin\Field\FieldType;
+  /**
+   * Field type properties definition.
+   *
+   * Inside this method we define all the fields (properties) that our
+   * custom field type will have.
+   *
+   * Here there is a list of allowed property types: https://goo.gl/sIBBgO
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties['value'] = DataDefinition::create('integer')
+      ->setLabel(t('Stamboeknummer value'));
 
+    return $properties;
+  }
 
-class StamboeknummerItem {
-
+  /**
+   * Define when the field type is empty.
+   *
+   * This method is important and used internally by Drupal. Take a moment
+   * to define when the field fype must be considered empty.
+   */
+  public function isEmpty() {
+    $value = $this->get('value')->getValue();
+    return $value === NULL || $value === '';
+  }
 }
