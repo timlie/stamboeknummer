145771b9d475a1890871ee8e6ec41ef89e5333c4
Tim Lievens <timlievens@gmail.com>
2016-11-17 20:32:53 +0100

    Cleanup code

--- src/Plugin/Validation/Constraint/ModuloConstraintValidator.php
+++ src/Plugin/Validation/Constraint/ModuloConstraintValidator.php
@@ -15,11 +15,16 @@ class ModuloConstraintValidator extends ConstraintValidator {
     if (!isset($value)) {
       return;
     }
-    // Verify that given value is even.
+
+    /*
+     * Stamboeknummer is valid when the modulo (97) of the first 9 digits are
+     * equal to the last two digits of the stamboeknummer.
+     */
+    // Get first 9 digits and take modulo 97.
     $modulo = (intval($value / 100) % 97);
-    print_r($modulo);
+    // Get last two digits
     $controlegetal = round((($value / 100) - intval($value / 100)) * 100);
-    print_r($controlegetal);
+    // If both are not equal, give error.
     if ($modulo != $controlegetal) {
       $this->context->addViolation($constraint->message, [
         '%value' => $value,
